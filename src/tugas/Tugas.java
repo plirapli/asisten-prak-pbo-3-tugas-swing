704e7d62c4dc470ddc4941af7e41ef2fcd8f1e7c
plirapli <rafliyulistiawan@gmail.com>
2024-03-13 21:50:42 +0700

    remove unnecessary

--- src/tugas/Tugas.java
+++ src/tugas/Tugas.java
@@ -1,12 +1,5 @@
 package tugas;
 
-import javax.swing.ButtonGroup;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JRadioButton;
-import javax.swing.JTextField;
-
 /**
  *
  * @author rafli
